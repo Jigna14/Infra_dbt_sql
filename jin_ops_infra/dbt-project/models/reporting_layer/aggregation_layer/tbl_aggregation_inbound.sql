869f90d49f0cce08736847efa0ddaff5c9e7d377
Jignasha Dobariya <97515701+Jigna14@users.noreply.github.com>
2025-02-05 11:23:47 +0100

    Update tbl_aggregation_inbound.sql

---
+++ jin_ops_infra/dbt-project/models/reporting_layer/aggregation_layer/tbl_aggregation_inbound.sql
@@ -0,0 +1,42 @@
+
+{{ config(materialized='incremental',
+              unique_key = ['surrogate_key'],
+              merge_update_columns=[
+                    'dwh_modified_timestamp',
+                    'metric_numerator',
+                    'metric_denominator'])}}
+
+
+
+WITH cte AS (
+
+SELECT
+    b.warehouse_name,
+    c.country_long,
+    '174'  AS metric_id, 
+    "Yard Utilization"  AS metric_name, 
+    CAST(a.report_date AS DATE) AS time_base_date,
+    amount_of_units AS metric_numerator,
+    a.yard_capacity AS metric_denominator,
+FROM {{ref('tbl_metric_yard_utilization')}} a
+LEFT JOIN {{ref('tbl_dim_castlegate_warehouse')}} b 
+    USING (warehouse_id)
+LEFT JOIN {{ref('tbl_dim_country')}} c 
+    USING (country_id)
+WHERE 1=1
+AND cast(report_date as date)=DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+
+    
+)
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['metric_id', 'metric_name', 'time_base_date', 'warehouse_name', 'country_long'])}} AS surrogate_key,
+    CURRENT_TIMESTAMP() AS dwh_creation_timestamp,
+    CURRENT_TIMESTAMP() AS dwh_modified_timestamp,
+    metric_id,
+    metric_name,
+    time_base_date,
+    warehouse_name,
+    country_long,
+    metric_numerator,
+    metric_denominator
+FROM cte
