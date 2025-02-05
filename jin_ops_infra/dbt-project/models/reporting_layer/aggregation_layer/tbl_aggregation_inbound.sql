b001069b8512e6365073b9fdd22ae9b1bc9cf3b5
Jignasha Dobariya <97515701+Jigna14@users.noreply.github.com>
2025-02-05 11:26:11 +0100

    Update tbl_aggregation_inbound.sql

--- jin_ops_infra/dbt-project/models/reporting_layer/aggregation_layer/tbl_aggregation_inbound.sql
+++ jin_ops_infra/dbt-project/models/reporting_layer/aggregation_layer/tbl_aggregation_inbound.sql
@@ -44,6 +44,25 @@ LEFT JOIN {{ref('tbl_dim_country')}} c
 WHERE 1=1
 AND report_date=DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 GROUP BY 1,2,3,4,5
+
+UNION ALL
+    
+SELECT
+    b.warehouse_name,
+    c.country_long,
+    '1474'  AS metric_id, 
+    "Trailers out of Compliance vs 72 Hr Yard Arrival to Unload SLA"  AS metric_name, 
+    CAST(a.report_date AS DATE) AS time_base_date,
+    SUM(a.out_of_arrival_72_hours) AS metric_numerator,
+    1 AS metric_denominator,
+FROM {{ref('tbl_metric_inbound_trailers')}} a
+LEFT JOIN {{ref('tbl_dim_castlegate_warehouse')}} b 
+    USING (warehouse_id)
+LEFT JOIN {{ref('tbl_dim_country')}} c 
+    USING (country_id)
+WHERE 1=1
+AND report_date=DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+GROUP BY 1,2,3,4,5
   
 )
 SELECT
