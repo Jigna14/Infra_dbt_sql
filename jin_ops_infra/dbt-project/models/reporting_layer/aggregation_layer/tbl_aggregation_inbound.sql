4f9138572b85f15eb3f041bbaf31d039d056aec9
Jignasha Dobariya <97515701+Jigna14@users.noreply.github.com>
2025-02-05 11:28:44 +0100

    Update tbl_aggregation_inbound.sql

--- jin_ops_infra/dbt-project/models/reporting_layer/aggregation_layer/tbl_aggregation_inbound.sql
+++ jin_ops_infra/dbt-project/models/reporting_layer/aggregation_layer/tbl_aggregation_inbound.sql
@@ -102,6 +102,25 @@ WHERE 1=1
 AND report_date=DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 GROUP BY 1,2,3,4,5
 
+UNION ALL
+    
+SELECT
+    b.warehouse_name,
+    c.country_long,
+    '527'  AS metric_id, 
+    "EOD IB Units on Yard & Doors"  AS metric_name, 
+    CAST(a.report_date AS DATE) AS time_base_date,
+    SUM(a.total_amount_units) AS metric_numerator,
+    1 AS metric_denominator,
+FROM {{ref('tbl_metric_inbound_trailers')}} a
+LEFT JOIN {{ref('tbl_dim_castlegate_warehouse')}} b 
+    USING (warehouse_id)
+LEFT JOIN {{ref('tbl_dim_country')}} c 
+    USING (country_id)
+WHERE 1=1
+AND report_date=DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+GROUP BY 1,2,3,4,5
+
 )
 SELECT
     {{ dbt_utils.generate_surrogate_key(['metric_id', 'metric_name', 'time_base_date', 'warehouse_name', 'country_long'])}} AS surrogate_key,
